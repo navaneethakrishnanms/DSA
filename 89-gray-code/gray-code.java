9d0d38861aa11776c1d9f93ca2471b319d88d8f7
NAVANEETHA KRISHNAN M S <navaneethakrishnanms19@gmail.com>
2026-09-13 21:54:14 +0530

    Time: 4 ms (99.64%) | Memory: 53.6 MB (60.15%) - LeetSync

---
+++ 89-gray-code/gray-code.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+        int totalCodes = 1 << n; 
+        
+        for (int i = 0; i < totalCodes; ++i) {
+            ans.add(i ^ (i >> 1));
+        }
+        
+        return ans;
+    }
+}
